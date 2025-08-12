e302864a910f47632295a8cf1429ba798badd20d
Jack Oliveira <jadsonolvr@gmail.com>
2025-08-12 20:14:17 -0300

    feat: bases

---
+++ backend.hcl
@@ -0,0 +1,4 @@
+bucket  = "devops-it-tfstates-sa-east-1"
+key     = "terraform/devops-it.tfstate"
+region  = "sa-east-1"
+encrypt = true
---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = { source = "hashicorp/aws", version = ">= 5.0" }
+  }
+  backend "s3" {} # parâmetros via backend.hcl
+}
+
+provider "aws" {
+  region = var.region
+  default_tags = {
+    Project = "devops-it"
+    ManagedBy = "terraform"
+  }
+}

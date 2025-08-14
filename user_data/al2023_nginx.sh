8b1782b3858021cf716867e2548e3ef93b9e6c7b
Jack Oliveira <jadsonolvr@gmail.com>
2025-08-13 22:50:35 -0300

    video explicativo

--- user_data/al2023_nginx.sh
+++ user_data/al2023_nginx.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 dnf -y update
 dnf -y install nginx
-echo "OK - devops-it" > /usr/share/nginx/html/index.html
+echo "OK - video Expolicativo" > /usr/share/nginx/html/index.html
 systemctl enable nginx
 systemctl restart nginx
